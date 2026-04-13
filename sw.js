3bb3210b67ba1b5eee265cc9f6354d289b0a950b
redeyesoklm <clement.renois@gmail.com>
2026-04-13 07:24:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app').then(cache => cache.addAll(['./','index.html']))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
